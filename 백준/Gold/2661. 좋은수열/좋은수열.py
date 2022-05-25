92849bf0e4a25c27a36df9283d44ea9105cbe1bc
heitzes <66466798+heitzes@users.noreply.github.com>
2022-05-25 12:23:57 +0900

    Update 좋은수열.py

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -18,18 +18,23 @@ def check(_list, leng):
 
 def dfs(_list):
     global ans
+    # 제약조건: 중복되는 인접 수열이 있으면 안됨 -> 이전 순열로 돌아감
     if len(_list) >= 1:
         repeat = check(_list, len(_list))
         if repeat:
             return
+    # 종료조건: 길이가 N이 되면 종료
     if len(_list) == N:
         ans = int(''.join(_list))
         return
     for n in nums:
         _list.append(n)
         dfs(_list)
+        # 길이가 N이 되면 가장 작은 좋은 순열을 찾은 것이므로 더이상 순열을 찾아보지 않아도 됨
+        # 만약 check로 중복을 확인하지 않으면 N==2일때 오류생김
         if len(_list) == N and not check(_list, len(_list)):
             return
+        # 11 같이 제약조건에 걸리는 순열일 경우 pop해서 이전 순열인 1로 돌아감
         _list.pop()
 alist = []
 dfs(alist)
