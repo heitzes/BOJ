3f74fd4298b7e36d55c794b31464e75c9744e900
heitzes <66466798+heitzes@users.noreply.github.com>
2022-05-25 11:06:18 +0900

    Update 외판원 순회 2.py

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -3,14 +3,16 @@ from itertools import permutations
 
 def input():
     return sys.stdin.readline().rstrip()
+# permutation 쓰는 방법과 dfs 쓰는 방법 두가지 존재
 
+# permutation 풀이
 N = int(input())
 city = [list(map(int, input().split())) for _ in range(N)]
 select = [i for i in range(N)]
-#perm = list(permutations(select, N))
-#print(len(perm))
 mini = 100000000
-for i in (permutations(select, N)):
+
+# list(permutations(select, N)) 사용하면 메모리 부족 뜸 !!!! 리스트가 메모리를 많이 잡아먹는듯
+for i in permutations(select, N):
     test1 = list(i)
     test2 = test1[1:] + [test1[0]]
     cnt = 0
@@ -22,3 +24,30 @@ for i in (permutations(select, N)):
     else:
         mini = min(mini, cnt)
 print(mini)
+
+# dfs 풀이
+N = int(input())
+city = [list(map(int, input().split())) for _ in range(N)]
+ans = []
+_list = []
+def dfs(cnt, _list):
+    if len(_list) >= 2:
+        if city[_list[-2]][_list[-1]] == 0:
+            return
+        else:
+            cnt += city[_list[-2]][_list[-1]]
+    if len(_list) == N:
+        if city[_list[-1]][_list[0]] == 0:
+            return
+        else:
+            cnt += city[_list[-1]][_list[0]]
+            ans.append(cnt)
+            cnt = 0
+            return cnt
+    for i in range(N):
+        if i not in _list:
+            _list.append(i)
+            dfs(cnt, _list)
+            _list.pop()
+dfs(0, _list)
+print(min(ans))
