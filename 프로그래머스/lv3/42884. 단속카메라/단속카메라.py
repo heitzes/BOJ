cafdfe25e481c5204df11b42eecfc4f04052fdde
heitzes <66466798+heitzes@users.noreply.github.com>
2022-09-21 13:41:04 +0900

    [level 3] Title: 단속카메라, Time: 9.84 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,21 @@
+def solution(routes):
+    answer = 1
+    routes = sorted(routes, key = lambda x: (x[0], x[1]))
+    cur, ref = min(routes[0]), max(routes[0])
+    print(routes)
+    while True:
+        route = routes.pop(0)
+        start, end = min(route), max(route)
+        if cur <= start:
+            if start <= ref:
+                cur = start
+                ref = min(end, ref)
+            else:
+                answer += 1
+                cur = start
+                ref = end
+        print(cur, ref, answer)
+        if len(routes) == 0:
+            break
+        
+    return answer
