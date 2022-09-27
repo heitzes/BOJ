9d2551536adc4f58ae668ad11db283d8bc3a2f79
heitzes <66466798+heitzes@users.noreply.github.com>
2022-09-27 22:32:16 +0900

    Update 등산코스 정하기.py

--- 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,41 +1,45 @@
 from collections import defaultdict
 import heapq
 def solution(n, paths, gates, summits):
-    answer = []
     childs = defaultdict(list)
     for a, b, c in paths:
+        ## 헤맸던 부분
+        ## inten, child를 구분하여 너무 많은 자료구조를 만듬 -> 시간초과
+        ## child 안에 intensity까지 한번에 저장
         childs[a].append([b,c])
         childs[b].append([a,c])
     summits, gates = set(summits), set(gates)
     ans = [10000001] * (n+1)
+    ## 헤맸던 부분 
+    ## 1. gates와 summits에 대한 이중 for문을 돌렸음 
+    ## 2. summits에 대해 먼저 for문을 돌렸음
     for gate in gates:
         ans = dijkstra(ans, n, childs, gate, summits, gates)
     answer = []
     maxi = ans[0]
     summits = sorted(summits, reverse=True)
     for summ in summits:
-        if ans[summ] <= maxi:
-            answer = [summ, ans[summ]]
-            maxi = ans[summ]
-    return answer
+        answer.append([summ, ans[summ]])
+    answer = sorted(answer, key = lambda x: (x[-1], x[0]))
+    return answer[0]
 
 def dijkstra(ans, n, link, node, summits, gates):
     ans[node] = 0
     hq = []
+    ## 헤맸던 부분
+    ## heapq 대신 deque를 쓰고 deque를 정렬한 다음 popleft를 하는 방식으로 우선순위를 정했는데
+    ## heapq를 사용하면 알아서 정렬되기 때문에 시간복잡도가 더 작음
     heapq.heappush(hq, [0, node])
-    cnt = 0
     while hq:
-        cnt += 1
-        val, x = heapq.heappop(hq)
+        _, x = heapq.heappop(hq)
         if x in summits:
-            continue
-        if ans[x] < val:
-            continue
-        for ch,i in link[x]:
+            break
+        for ch, i in link[x]:
             if ch not in gates:
                 cost = max(ans[x], i)
+                ## 헤맸던 부분
+                ## 다른 노드 거쳐서 가는게 더 짧을때만 heapq에 push해야 함!
                 if cost < ans[ch]:
                     ans[ch] = cost
                     heapq.heappush(hq, [ans[ch], ch])
     return ans
-            
