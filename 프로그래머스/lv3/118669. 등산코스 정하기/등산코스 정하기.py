fd223ef610ce62e27de4a537381fd4bcffbfb005
heitzes <66466798+heitzes@users.noreply.github.com>
2022-09-27 22:50:53 +0900

    Update 등산코스 정하기.py

--- 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -4,15 +4,15 @@ def solution(n, paths, gates, summits):
     childs = defaultdict(list)
     for a, b, c in paths:
         ## 헤맸던 부분
-        ## inten, child를 구분하여 너무 많은 자료구조를 만듬 -> 시간초과
-        ## child 안에 intensity까지 한번에 저장
+        ## inten, child를 구분하여 너무 많은 자료구조를 만듬 -> 시간초과 -> child 안에 intensity까지 한번에 저장!
         childs[a].append([b,c])
         childs[b].append([a,c])
     summits, gates = set(summits), set(gates)
     ans = [10000001] * (n+1)
     ## 헤맸던 부분 
     ## 1. gates와 summits에 대한 이중 for문을 돌렸음 
-    ## 2. summits에 대해 먼저 for문을 돌렸음
+    ## -> 어차피 가장 작은 intensity를 구하는 것이기 때문에 변경된 ans를 다시 사용해도 되고, 모든 gates에 대해 dijkstra가 완료된후에 최소값을 찾으면 됨
+    ## 2. summits에 대해 dijkstra를 진행함 -> intensity가 동일한 경우를 처리하지 못함 (23번째 줄 참고)
     for gate in gates:
         ans = dijkstra(ans, n, childs, gate, summits, gates)
     answer = []
@@ -38,7 +38,7 @@ def dijkstra(ans, n, link, node, summits, gates):
             if ch not in gates:
                 cost = max(ans[x], i)
                 ## 헤맸던 부분
-                ## 다른 노드 거쳐서 가는게 더 짧을때만 heapq에 push해야 함!
+                ## 항상 heapq에 push하는게 아니라 다른 노드 거쳐서 가는게 더 짧은 경우에만 heapq에 push해야 함!
                 if cost < ans[ch]:
                     ans[ch] = cost
                     heapq.heappush(hq, [ans[ch], ch])
