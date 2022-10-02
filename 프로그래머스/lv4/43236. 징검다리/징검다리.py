e568e5db9f9603604e289b59e9b28ffda1aaf0c9
heitzes <66466798+heitzes@users.noreply.github.com>
2022-10-02 10:55:50 +0900

    [level 4] Title: 징검다리, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv4/43236. 징검다리/징검다리.py
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.py
@@ -1,34 +1,19 @@
 from collections import deque
 def solution(distance, rocks, n):
-    answer = 0
-    rocks = deque(sorted(rocks))
-    rocks.appendleft(0)
-    rocks.append(distance)
+    sorted_rocks = sorted(rocks)
+    sorted_rocks.append(distance)
     left, right = 0, distance
-    while left < right:
+    while left <= right:
+        cur, cnt = 0, 0
         mid = (left + right) // 2
-        if func(rocks, n, mid) < mid:
-            right = mid
+        for i in sorted_rocks:
+            if i - cur < mid:
+                cnt += 1
+            else:
+                cur = i
+        if cnt > n:
+            right = mid - 1
         else:
             left = mid + 1
             answer = mid
     return answer
-
-def func(rocks, n, dist):
-    cnt = 0
-    i = 0
-    ref = 0
-    mini = 1000000000
-    while cnt <= n and i < len(rocks)-1:
-        cur = rocks[i+1] - rocks[i] + ref
-        if cur >= dist:
-            ref = 0
-            mini = min(mini, cur)
-        else:
-            cnt += 1
-            ref += rocks[i+1] - rocks[i]
-        i += 1
-    if cnt <= n and i == len(rocks)-1:
-        return mini
-    else:
-        return 0
