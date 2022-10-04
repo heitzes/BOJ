37d00974f0dde387377aaf0318e70c50da2f764b
heitzes <66466798+heitzes@users.noreply.github.com>
2022-10-04 13:08:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 296 ms, Memory: 193672 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,17 +3,12 @@ def input():
     return sys.stdin.readline().rstrip()
     
 N, K = map(int, input().split())
-dp = {}
-dp[0] = 0
-for _ in range(N):
+dp = [[0]*(K+1) for _ in range(N+1)]
+for i in range(1,N+1):
     w, v = map(int, input().split())
-    ref = dp.copy()
-    for tw, tv in ref.items():
-        if w+tw <= K:
-            if w+tw not in dp:
-                dp[w+tw] = v+tv
-            else:
-                prev = dp[w+tw]
-                curr = tv + v
-                dp[w+tw] = max(prev, curr)
-print(max(dp.values()))
+    for j in range(K, 0, -1):
+        if j >= w:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
+        else:
+            dp[i][j] = dp[i-1][j]
+print(dp[N][K])
