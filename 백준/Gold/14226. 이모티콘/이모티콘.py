ad48db6224930b966f265eb75beaba27ccab992e
heitzes <66466798+heitzes@users.noreply.github.com>
2022-10-04 16:23:03 +0900

    [Gold IV] Title: 이모티콘, Time: 372 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -0,0 +1,18 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+
+s = int(input())
+
+dp = [i for i in range(s+1)]
+for i in range(2, s+1):
+    if i % 2:
+        dp[i] = min(dp[i], dp[i//2+1]+3)
+    else:
+        dp[i] = min(dp[i], dp[i//2]+2)
+    for j in range(i+1, s+1):
+        if j % i == 0:
+            dp[j] = min(dp[j], dp[i]+1+(j//i-1))
+        else:        
+            dp[j] = min(dp[j], dp[i]+1+(j//i)+((i)*(j//i+1)-j))
+print(dp[-1])
