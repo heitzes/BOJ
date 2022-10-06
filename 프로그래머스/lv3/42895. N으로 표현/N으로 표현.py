b5c667e711e966ee06527efa5fb26e0514852504
heitzes <66466798+heitzes@users.noreply.github.com>
2022-10-06 20:45:24 +0900

    [level 3] Title: N으로 표현, Time: 6.32 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,24 @@
+def solution(N, number):
+    dp = [set() for _ in range(10)]
+    dp[0].add(N)
+    dp[1].add(N)
+    for i in range(2, 9):
+        for j in range(1, i//2+1):
+            for a in dp[j]:
+                for b in dp[i-j]:
+                    dp[i].add(a+b)
+                    dp[i].add(a*b)
+                    dp[i].add(abs(a-b))
+                    if a > b:
+                        if b!=0:
+                            dp[i].add(a//b)
+                    else:
+                        if a!=0:
+                            dp[i].add(b//a)
+                    dp[i].add(int(str(N)*(i)))
+                    
+    for i in range(1,9):
+        s = dp[i]
+        if number in s:
+            return i
+    return -1
