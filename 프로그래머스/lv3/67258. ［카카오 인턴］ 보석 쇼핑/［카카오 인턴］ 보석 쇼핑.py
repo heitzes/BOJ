7ea3ffef44eefd986d12df4cfc76b16ab82a964d
heitzes <66466798+heitzes@users.noreply.github.com>
2022-10-08 19:11:43 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 121.03 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -17,15 +17,5 @@ def solution(gems):
             else:
                 answer.append([e-s, s+1, e+1])
                 e += 1
-    # while s <= e and e < len(gems):
-    #     count = Counter(gems[s:e+1])
-    #     if len(count) < len(shop):
-    #         e += 1
-    #     elif len(count) == len(shop):
-    #         if count[gems[s]] > 1:
-    #             s += 1
-    #         else:
-    #             answer.append([e-s, s+1, e+1])
-    #             e += 1
     answer.sort()
     return answer[0][1:]
