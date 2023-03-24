65ce3e175564da4a1339e1bf34ae0bf84aa75e12
Solphi a.k.a Orca <66466798+heitzes@users.noreply.github.com>
2023-03-24 16:08:23 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,28 +1,27 @@
 from collections import defaultdict
 def solution(survey, choices):
-    score = [0, 3, 2, 1, 0, 1, 2, 3, 0]
-    save = defaultdict(int)
     answer = ''
-    for i in range(len(survey)):
-        s, c = survey[i], choices[i]
-        if c > 4:
-            save[s[1]] += score[c]
+    # 왼쪽에 있는게 비동의, 오른쪽에 있는게 동의 점수
+    # 중앙값 4와 비교해서 점수 부여 (4보다 작으면 왼쪽, 4보다 크면 오른쪽)
+    # 'AN'에 선택 5 -> 1점을 N에 부여
+    # 동일 점수라면 사전순으로
+    one = {'R':0, 'T':0}
+    two = {'C':0, 'F':0}
+    three = {'J':0, 'M':0}
+    four = {'A':0, 'N':0}
+    find = {'R': 0, 'T':0, 'C':1, 'F':1, 'J':2, 'M':2, 'A':3, 'N':3}
+    test = [one, two, three, four]
+    
+    for s, ch in zip(survey, choices):
+        point = test[find[s[0]]]
+        if ch > 4:
+            point[s[1]] += ch - 4
         else:
-            save[s[0]] += score[c]
-    if save['R'] >= save['T']:
-        answer += 'R'
-    else:
-        answer += 'T'
-    if save['C'] >= save['F']:
-        answer += 'C'
-    else:
-        answer += 'F'
-    if save['J'] >= save['M']:
-        answer += 'J'
-    else:
-        answer += 'M'
-    if save['A'] >= save['N']:
-        answer += 'A'
-    else:
-         answer += 'N'
+            point[s[0]] += 4 - ch
+    
+    for t in test:
+        result = sorted(t.items(), key = lambda x: -x[-1])
+        answer += result[0][0]
+        
     return answer
+    
